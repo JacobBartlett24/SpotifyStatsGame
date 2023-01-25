d986053f711fdd1d0e236c9ed9049f5653cd32d4
JacobBartlett24 <jacobart@ttu.edu>
2023-01-25 10:39:11 -0600

    asd

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ const Login = () =>{
         let token = window.localStorage.getItem('token');
 
         if(!token && hash){
-            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+            token = hash.substring(1).split("&").finsd(elem => elem.startsWith("access_token")).split("=")[1]
 
             window.location.hash = ''
             window.localStorage.setItem("token",token)
